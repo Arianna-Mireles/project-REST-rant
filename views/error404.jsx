037710f71488094248bf2ed8a02db88f80616f43
Arianna Mireles <arianna.mireles02@gmail.com>
2023-10-27 17:31:44 -0500

    part 4

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,7 +7,7 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
-              <img src="/images/lion-404.jpg" alt="An up close pisture of a lion with it's mouth open" width="600" height="1000" />
+              <img src="/images/lion-404.jpg" alt="An up close picture of a lion with it's mouth open" width="600" height="1000" />
               <div>
                 Photo by <a href="https://unsplash.com/@rodlong?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Rod Long</a> on <a href="https://unsplash.com/photos/a-close-up-of-a-lions-face-with-a-black-background-LLLMIdYhbCY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
               </div>
