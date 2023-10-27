f1655c5c1c8a564295ba9ee87c1048af6daea3c3
Arianna Mireles <arianna.mireles02@gmail.com>
2023-10-27 15:56:53 -0500

    add image to error page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,10 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <img src="/images/lion-404.jpg" alt="An up close pisture of a lion with it's mouth open" />
+              <div>
+                Photo by <a href="https://unsplash.com/@rodlong?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Rod Long</a> on <a href="https://unsplash.com/photos/a-close-up-of-a-lions-face-with-a-black-background-LLLMIdYhbCY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+              </div>
           </main>
       </Def>
     )
