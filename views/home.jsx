72ff96e3128786759dc6b628a963b1791c0a7092
Arianna Mireles <arianna.mireles02@gmail.com>
2023-10-27 15:40:24 -0500

    add image to home page

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img src="/images/red-wine.jpg" alt="Glass of red wine" />
+                <div>
+                  Photo by <a href="https://unsplash.com/@aliissinisalu?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Aliis Sinisalu</a> on <a href="https://unsplash.com/">Unsplash</a>
+                </div>
+              </div>
               <a href="/places">
                 <button className="btn-primary">Places Page</button>
               </a>
