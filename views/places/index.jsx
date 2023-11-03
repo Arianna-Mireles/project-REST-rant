db43fc45e3be6e1d48ab7ac854a956cb1955f4aa
Arianna Mireles <arianna.mireles02@gmail.com>
2023-11-02 19:30:05 -0500

    part 4

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -4,15 +4,11 @@ const Def = require('../default')
 function index (data) {
     let placesFormatted = data.places.map((place) => { 
         return (
-            <div className="col-sm">
+            <div className="col-sm-6">
                 <h2>{place.name}</h2>
-                <p className="text-center">
-                    {place.cuisines}
-                </p>
-                <img src={place.pic} alt={place.name}/>
-                <p className="text-center">
-                    Located in {place.city}, {place.state}
-                </p>
+                <p className="text-center">{place.cuisines}</p>
+                <img className="col-sm-6" src={place.pic} alt={place.name}/>
+                <p className="text-center">Located in {place.city}, {place.state}</p>
             </div>
         )
     })
