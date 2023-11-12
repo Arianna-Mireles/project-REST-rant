13810c7a9e8d470d680104a0059c425bde7888c5
Arianna Mireles <arianna.mireles02@gmail.com>
2023-11-12 00:11:04 -0600

    Create and use a seeder file and helper method

--- models/places.js
+++ models/places.js
@@ -1,16 +1,3 @@
-// module.exports = [{
-//     name: 'H-Thai-ML',
-//     city: 'Seattle',
-//     state: 'WA',
-//     cuisines: 'Thai, Pan-Asian',
-//     pic: '/images/h-thai-ml.jpg'
-// }, {
-//     name: 'Coding Cat Cafe',
-//     city: 'Phoenix',
-//     state: 'AZ',
-//     cuisines: 'Coffee, Bakery',
-//     pic: '/images/cat-cafe.jpg'
-// }]
 const mongoose = require('mongoose')
 
 const placeSchema = new mongoose.Schema({
@@ -22,5 +9,10 @@ const placeSchema = new mongoose.Schema({
   founded: Number
 })
 
+placeSchema.methods.showEstablished = function() {
+  return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}.`
+}
+
 module.exports = mongoose.model('Place', placeSchema)
+
 
